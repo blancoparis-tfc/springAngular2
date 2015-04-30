6607cae039d46ff4a898e91c853fd7c7ca1b8540
blancoparis-tfc <blancoparis.tfc@gmail.com>
2015-04-30 23:08:04 +0200

    Configuración del aspecto
    Configuramos el aspecto para el sistema de logs.

--- src/main/java/org/dbp/conf/WebConfig.java
+++ src/main/java/org/dbp/conf/WebConfig.java
@@ -1,9 +1,11 @@
 package org.dbp.conf;
 
+import org.dbp.conf.aop.log.LogAspect;
 import org.dbp.conf.interceptor.LogInterceptor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.EnableAspectJAutoProxy;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
@@ -11,12 +13,21 @@ import org.springframework.web.servlet.view.InternalResourceViewResolver;
 
 @Configuration
 @EnableWebMvc
+@EnableAspectJAutoProxy
 @ComponentScan(
 		basePackages={
 				"org.dbp.controller"	// Es donde se ubicaran los controladores
+				//,"org.dbp.conf.aop.log" // Activar los aspecto de los logs.
 				}
 		)
+
 public class WebConfig extends WebMvcConfigurerAdapter{
+	
+	@Bean
+	public LogAspect logAspect(){
+		return new LogAspect();
+	}
+	
 	/**
 	 * 
 	 * Configuramos que las paginas se guarden en la carpeta pages
--- src/main/java/org/dbp/controller/InicioController.java
+++ src/main/java/org/dbp/controller/InicioController.java
@@ -1,8 +1,12 @@
 package org.dbp.controller;
 
+import javax.servlet.http.HttpServletRequest;
+
+import org.dbp.conf.aop.log.DbpLog;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
@@ -10,11 +14,18 @@ import org.springframework.web.bind.annotation.RequestMapping;
 public class InicioController {
 
 	private static Logger logger = LoggerFactory.getLogger(InicioController.class);
-	
+	@DbpLog
 	@RequestMapping
 	public String inicio(){
 		logger.info("Metodo {}","inicio");
 		return "inicio";
+	}
+	
+
+	
+	@RequestMapping("excepcion/{param}")
+	public String excepcion(HttpServletRequest  request,@PathVariable(value="param") String param) throws Exception{
+		throw new Exception("Error de prueba");
 	}
 	
 }

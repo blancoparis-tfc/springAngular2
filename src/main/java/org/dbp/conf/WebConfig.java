9ac879ffb1e17eb66952fc29ea4b8ef4a4ad1bc3
blancoparis-tfc <blancoparis.tfc@gmail.com>
2015-05-02 10:55:38 +0200

    Terminar la configuración de JACKSON

--- src/main/java/org/dbp/conf/WebConfig.java
+++ src/main/java/org/dbp/conf/WebConfig.java
@@ -1,15 +1,62 @@
 package org.dbp.conf;
 
+import java.text.SimpleDateFormat;
+import java.time.Duration;
+import java.time.Instant;
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.time.LocalTime;
+import java.time.MonthDay;
+import java.time.OffsetDateTime;
+import java.time.OffsetTime;
+import java.time.Period;
+import java.time.Year;
+import java.time.YearMonth;
+import java.time.ZoneId;
+import java.time.ZoneOffset;
+import java.time.ZonedDateTime;
+import java.time.format.DateTimeFormatter;
+import java.util.List;
+
 import org.dbp.conf.aop.log.LogAspect;
 import org.dbp.conf.interceptor.LogInterceptor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.EnableAspectJAutoProxy;
+import org.springframework.http.converter.HttpMessageConverter;
+import org.springframework.http.converter.json.Jackson2ObjectMapperBuilder;
+import org.springframework.http.converter.json.MappingJackson2HttpMessageConverter;
+import org.springframework.http.converter.xml.MappingJackson2XmlHttpMessageConverter;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
 import org.springframework.web.servlet.view.InternalResourceViewResolver;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.module.SimpleModule;
+import com.fasterxml.jackson.databind.ser.std.ToStringSerializer;
+import com.fasterxml.jackson.datatype.jsr310.JSR310Module;
+import com.fasterxml.jackson.datatype.jsr310.PackageVersion;
+import com.fasterxml.jackson.datatype.jsr310.deser.DurationDeserializer;
+import com.fasterxml.jackson.datatype.jsr310.deser.InstantDeserializer;
+import com.fasterxml.jackson.datatype.jsr310.deser.JSR310StringParsableDeserializer;
+import com.fasterxml.jackson.datatype.jsr310.deser.LocalDateDeserializer;
+import com.fasterxml.jackson.datatype.jsr310.deser.LocalDateTimeDeserializer;
+import com.fasterxml.jackson.datatype.jsr310.deser.LocalTimeDeserializer;
+import com.fasterxml.jackson.datatype.jsr310.deser.OffsetTimeDeserializer;
+import com.fasterxml.jackson.datatype.jsr310.deser.YearDeserializer;
+import com.fasterxml.jackson.datatype.jsr310.deser.YearMonthDeserializer;
+import com.fasterxml.jackson.datatype.jsr310.ser.DurationSerializer;
+import com.fasterxml.jackson.datatype.jsr310.ser.InstantSerializer;
+import com.fasterxml.jackson.datatype.jsr310.ser.LocalDateSerializer;
+import com.fasterxml.jackson.datatype.jsr310.ser.LocalDateTimeSerializer;
+import com.fasterxml.jackson.datatype.jsr310.ser.LocalTimeSerializer;
+import com.fasterxml.jackson.datatype.jsr310.ser.OffsetDateTimeSerializer;
+import com.fasterxml.jackson.datatype.jsr310.ser.OffsetTimeSerializer;
+import com.fasterxml.jackson.datatype.jsr310.ser.YearMonthSerializer;
+import com.fasterxml.jackson.datatype.jsr310.ser.YearSerializer;
+import com.fasterxml.jackson.datatype.jsr310.ser.ZonedDateTimeSerializer;
 
 @Configuration
 @EnableWebMvc
@@ -47,7 +94,47 @@ public class WebConfig extends WebMvcConfigurerAdapter{
 		super.addInterceptors(registry);
 		registry.addInterceptor(new LogInterceptor());
 	}
+
+	
+	private  class LocalDateDeserializerEs  extends LocalDateDeserializer{
+		private LocalDateDeserializerEs(){
+			super(DateTimeFormatter.ofPattern("dd/MM/yyyy"));
+		}
+	}
+	
+	private class LocalDateSerializerEs extends LocalDateSerializer{
+		private  LocalDateSerializerEs(){
+			super(false,DateTimeFormatter.ofPattern("dd/MM/yyyy"));
+		}
+	}
+	
+	private class JSR310ModuleEs extends SimpleModule{
+		public JSR310ModuleEs() {
+			super(PackageVersion.VERSION);
+	        addDeserializer(LocalDate.class,new LocalDateDeserializerEs());
+	        addSerializer(LocalDate.class, new LocalDateSerializerEs());
+		}
+	}
+
+	@Override
+	public void configureMessageConverters(
+			List<HttpMessageConverter<?>> converters) {
+		super.configureMessageConverters(converters);
+		Jackson2ObjectMapperBuilder builder = new Jackson2ObjectMapperBuilder();
+	    builder
+	    	.indentOutput(true)
+	    	.dateFormat(new SimpleDateFormat("dd-MM-yyyy")
+	    	);
+	    converters.add(new MappingJackson2HttpMessageConverter(
+	    				builder
+	    					.build()
+	    					.registerModule(new JSR310ModuleEs())));
+	    converters.add(new MappingJackson2XmlHttpMessageConverter(
+	    				builder.createXmlMapper(true)
+	    					.build()
+	    					.registerModule(new JSR310ModuleEs())));
+	}
     
-    
+ 
     
 }
--- src/main/java/org/dbp/controller/json/EjemploJson.java
+++ src/main/java/org/dbp/controller/json/EjemploJson.java
@@ -1,11 +1,55 @@
 package org.dbp.controller.json;
 
 import java.io.Serializable;
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.util.Date;
+
+import com.fasterxml.jackson.annotation.JsonView;
 
 public class EjemploJson implements Serializable{
 
+	public interface Resumen{};
+
+	public interface Resumen2 extends Resumen{}
+	@JsonView(Resumen.class)
 	private Long numero;
+	
 	private String cadena;
+	@JsonView(Resumen2.class)
+	private String descripcion;
+	
+	private Date fecha;
+	
+	private LocalDate fechaLocal;
+	
+	private LocalDateTime fechaTime;
+	
+	
+	public LocalDateTime getFechaTime() {
+		return fechaTime;
+	}
+	public void setFechaTime(LocalDateTime fechaTime) {
+		this.fechaTime = fechaTime;
+	}
+	public Date getFecha() {
+		return fecha;
+	}
+	public void setFecha(Date fecha) {
+		this.fecha = fecha;
+	}
+	public LocalDate getFechaLocal() {
+		return fechaLocal;
+	}
+	public void setFechaLocal(LocalDate fechaLocal) {
+		this.fechaLocal = fechaLocal;
+	}
+	public String getDescripcion() {
+		return descripcion;
+	}
+	public void setDescripcion(String descripcion) {
+		this.descripcion = descripcion;
+	}
 	public Long getNumero() {
 		return numero;
 	}
@@ -22,6 +66,5 @@ public class EjemploJson implements Serializable{
 	public String toString() {
 		return "EjemploJson [numero=" + numero + ", cadena=" + cadena + "]";
 	}
-	
 	
 }
